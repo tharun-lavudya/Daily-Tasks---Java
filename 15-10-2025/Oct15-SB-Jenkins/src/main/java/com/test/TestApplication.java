e461749e31236afb005d75dcf3f6e24fd1f3aa47
tharun-lavudya <tharun.lavudya@encora.com>
2025-10-16 12:25:43 +0530

    Update TestApplication.java

--- 15-10-2025/Oct15-SB-Jenkins/src/main/java/com/test/TestApplication.java
+++ 15-10-2025/Oct15-SB-Jenkins/src/main/java/com/test/TestApplication.java
@@ -3,7 +3,7 @@ package com.test;
 public class TestApplication {
 	public static void main(String[] args) {
 		
-	
+	System.out.println("Hello World!");
 	System.out.println("Hello from Github Actions");
 	
 	}
@@ -13,5 +13,6 @@ public class TestApplication {
 	
 
 }
+
 
 
